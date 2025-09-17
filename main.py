b3ba09a7217ad4774e17c59972dbaeac79cda3ee
vhreisswitz <victorfigueirareisswitz@gmail.com>
2025-09-17 11:09:06 -0300

    como fazer o browser rodar

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QLineEdit
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
+#codigo para intalar PyQt5 e PyQtWebEngine no cmd para poder rodar o script
+# pip install PyQt5 PyQtWebEngine
 
 class MainWindow(QMainWindow):
     def __init__(self):
